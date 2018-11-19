754205df32ee236a0ac582332070ad18056051c4
fantastic-page-5704 <chandvakil@gmail.com>
2018-11-19 19:29:28 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if(string.toLowerCase()) === string) {
+    return "I can't hear you!"
+  }
+  
+}
+
+
+
+function testNum(a) {
+  if (a > 0) {
+    return "positive";
+  } else {
+    return "NOT positive";
+  }
+}
